095b6519d6301e0c45e3c50919701359b0a75648
Andrei Yudin <at-4@mail.ru>
2020-06-30 12:35:39 +0300

    Init commit, функционал таймера обратного отсчета для сайта

---
+++ script/script.js
@@ -0,0 +1,60 @@
+window.addEventListener('DOMContentLoaded', () => {
+    function countTimer(deadline) {
+        const timerHours = document.querySelector('#timer-hours');
+        const timerMinutes = document.querySelector('#timer-minutes');
+        const timerSeconds = document.querySelector('#timer-seconds');
+
+        function getTimeRemaining() {
+            const dateStop = new Date(deadline).getTime();
+            const dateNow = new Date().getTime();
+            const timeRemaining = (dateStop - dateNow) / 1000;
+            let seconds = Math.floor(timeRemaining % 60);
+            let minutes = Math.floor((timeRemaining / 60) % 60);
+            let hours = Math.floor(timeRemaining / 60 / 60);
+
+            if (hours < 10) {
+                hours = '0' + hours;
+            }
+
+            if (minutes < 10) {
+                minutes = '0' + minutes;
+            }
+
+            if (seconds < 10) {
+                seconds = '0' + seconds;
+            }
+
+            return { timeRemaining, hours, minutes, seconds };
+        }
+
+        function updateClock() {
+            const timer = getTimeRemaining();
+
+            if (timer.timeRemaining <= 0) {
+                return false;
+            }
+
+            timerHours.textContent = timer.hours;
+            timerMinutes.textContent = timer.minutes;
+            timerSeconds.textContent = timer.seconds;
+
+            return true;
+        }
+
+        function startTimer() {
+            const start = updateClock();
+
+            if (start) {
+                setInterval(updateClock, 1000);
+            } else {
+                timerHours.textContent = '00';
+                timerMinutes.textContent = '00';
+                timerSeconds.textContent = '00';
+            }
+        }
+
+        startTimer();
+    }
+
+    countTimer('1 july 2020');
+});
