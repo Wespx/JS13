57058162102db90c942f5e9fb4aa2ec7ada1018c
Andrei Yudin <at-4@mail.ru>
2020-07-07 15:47:10 +0300

    Lesson23 completed

--- script/script.js
+++ script/script.js
@@ -307,4 +307,34 @@ window.addEventListener('DOMContentLoaded', () => {
     };
 
     slider();
+
+    //обработчики инпутов калькулятора и фотографий
+
+    const eventListeners = () => {
+        const inputs = document.querySelectorAll('.calc-item');
+        const photos = document.querySelectorAll('.command__photo');
+        const switchPhoto = photo => {
+            const src = photo.getAttribute('src');
+            const dataImg = photo.getAttribute('data-img');
+
+            photo.setAttribute('src', dataImg);
+            photo.setAttribute('data-img', src);
+        };
+
+        inputs.forEach(input => {
+            input.addEventListener('input', e => {
+                e.target.value = e.target.value.replace(/\D/, '');
+            });
+        });
+
+        photos.forEach(photo => {
+            photo.addEventListener('mouseenter', switchPhoto.bind(this, photo));
+        });
+
+        photos.forEach(photo => {
+            photo.addEventListener('mouseleave', switchPhoto.bind(this, photo));
+        });
+    };
+
+    eventListeners();
 });
